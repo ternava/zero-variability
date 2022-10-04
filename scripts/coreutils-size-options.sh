4b160327944d62f0abc175fda9d6524eafdb0476
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2022-10-04 21:05:12 +0200

    add: coreutils size and options per program

---
+++ scripts/coreutils-size-options.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ ! -d "coreutils-9.1/" ] ; then
+    echo "coreutils-9.1 source does not exist"
+    echo "Downloading coreutils-9.1..."
+    curl -O https://ftp.gnu.org/gnu/coreutils/coreutils-9.1.tar.gz
+    echo "Decompressing coreutils-9.1.tar.gz..."
+    tar xf coreutils-9.1.tar.gz
+fi
+echo "coreutils-9.1 does exist, entering directory..."
+cd coreutils-9.1
+pgs=$(head -n 18 README | tail -n 9 | tr ' ' '\n' | grep -v install | sort | uniq)
+npgs=$(echo $pgs | tr ' ' '\n' | wc -l)
+echo "There are $npgs programs (without 'install')."
+
+if [ -f lib/config.h ] ; then
+    echo "Already configured."
+else
+    echo "Configure..."
+    ./configure --enable-install-program=arch,coreutils,hostname >> /dev/null
+fi
+
+if [ -f src/yes ] ; then
+    echo "Already built."
+else
+    echo "Building..."
+    make -j$(nproc) >> /dev/null
+fi
+
+# Quick check
+# for p in $pgs
+# do
+#     if [ -f src/$p ]; then
+#         echo "$p OK"
+#     else
+#         echo "$p KO"
+#     fi
+# done
+
+echo "program options size"
+
+for p in $pgs
+do
+    s=$(du -b src/$p | cut -f1)
+    f=$(./src/$p --help | grep -E "^((  )|(      ))(-|\+)((-?)([A-Za-z1-9]))" | wc -l)
+    if [ $f -eq 0 ]; then # If 0 options (e.g., "test"), consider at least
+                          # --help and --version
+        f=2
+    fi
+    printf "%s %s %s\n" "$p" "$s" "$f"
+done
